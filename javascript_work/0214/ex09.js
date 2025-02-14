42507729b74a434a9796f1de6fe98533f6d8f5f2
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-02-14 12:08:40 +0900

    no message

---
+++ javascript_work/0214/ex09.js
@@ -0,0 +1,16 @@
+const obj = { name: "홍길동", addr: "서울" };
+console.log(obj);
+
+const obj2 = { ...obj };
+console.log(obj2);
+
+// 문법 많이 X
+// 문법 많이 O
+// 객체 배열 문법...
+
+// 옛날 노력 XXX
+// 개발자 ..??
+
+const arr = [10, 20, 30];
+console.log(arr);
+console.log(...arr);
