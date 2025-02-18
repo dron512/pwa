0ada45a322d38337af224f9c66a105a41dc78955
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-02-18 12:50:48 +0900

    no message

---
+++ javascript_work/0218/ex13-1.js
@@ -0,0 +1,5 @@
+export const doa = function () {
+    console.log("test doa");
+}
+
+export default { aa: 10, bb: 20 };
---
+++ javascript_work/0218/ex13.js
@@ -0,0 +1,7 @@
+import de,{doa} from './ex13-1.js';
+
+export var a = 10;
+console.log(a);
+
+doa();
+console.log(de);
