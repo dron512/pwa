9eec189bca7baaee5f6131962cd5a33a9e3fbd10
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-02-25 16:05:45 +0900

    no message

--- javascript_work/0225/ex07.js
+++ javascript_work/0225/ex07.js
@@ -4,8 +4,8 @@ function Circle(radius) {
     return 'aa';
 }
 
-const ret1 = Circle(10);
-const ret2 = new Circle(5);
+const ret1 = Circle(10); // 반환되는 값
+const ret2 = new Circle(5); // 자동으로 {}
 
 console.log(ret1);
 console.log(ret2);
