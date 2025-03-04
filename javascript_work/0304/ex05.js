59819ab071da70ebaaa13480cf6ba490f5bcf858
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-03-04 12:20:41 +0900

    no message

---
+++ javascript_work/0304/ex05.js
@@ -0,0 +1,24 @@
+const defaultSettings = {
+    volume: 50,
+    brightness: 70,
+    name: "홍길동",
+};
+
+const userSettings = {
+    brightness: 90,
+    theme: 'dark'
+};
+
+const settings = { ...userSettings, ...defaultSettings };
+
+console.log(settings);
+// { volume: 50, brightness: 70, theme: 'dark' }
+
+const a = 10;
+// a = 20;
+
+const b = {};
+b.a = 10;
+console.log(b);
+
+// b = 30;
