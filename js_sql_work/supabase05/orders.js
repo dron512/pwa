c620b3a52b8d3e903becc8ebc278a97ab20c2106
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-03-11 15:59:03 +0900

    no message

--- js_sql_work/supabase05/orders.js
+++ js_sql_work/supabase05/orders.js
@@ -29,14 +29,41 @@ document.querySelector('#input-button-order').addEventListener('click', async fu
     console.log(res)
 })
 
+
+document.querySelector('#update-button-order').addEventListener('click', async function () {
+    const orderId = document.querySelector('#update-order-id').innerHTML;
+    const orderUserId = document.querySelector('#update-order-user-id').innerHTML;
+    const product_name = document.querySelector('#update-product-name').value;
+    const price = document.querySelector('#update-price').value;
+
+    const res = await supabase
+        .from('orders')
+        .update({
+            product_name,
+            price
+        })
+        .eq('id', orderId)
+        .select();
+    if (res.status == 200) {
+        const $modal = document.querySelector('#order-modal');
+        $modal.classList.add('hidden');
+        await Swal.fire({
+            title: "수정성공",
+            icon: "success",
+            draggable: true
+        });
+        ordersSelect();
+    }
+})
+
 async function ordersSelect() {
     const $orderDiv = document.querySelector('#orders-div');
 
-    const res = await supabase.from('orders').select();
+    const res = await supabase.from('orders').select().order('created_at', { ascending: false });
     let rows = '';
     for (let i = 0; i < res.data.length; i++) {
         rows = rows + `
-            <tr>
+            <tr style="cursor:pointer" onclick="orderRowClick(this);">
                 <td>${res.data[i].id}</td>
                 <td>${res.data[i].user_id}</td>
                 <td>${res.data[i].product_name}</td>
@@ -59,8 +86,23 @@ async function ordersSelect() {
         </table>
     </div>
     `;
-
     $orderDiv.innerHTML = orders;
     $orderDiv.classList.add('show');
+}
+
+function orderRowClick(trTag) {
+    // alert(trTag);
+    const $orderModal = document.querySelector('#order-modal');
+    $orderModal.classList.remove('hidden');
+
+    const orderId = trTag.children[0].innerText;
+    const userId = trTag.children[1].innerText;
+    const product_name = trTag.children[2].innerText;
+    const price = trTag.children[3].innerText;
+
+    document.querySelector('#update-order-id').innerHTML = orderId;
+    document.querySelector('#update-order-user-id').innerHTML = userId;
+    document.querySelector('#update-product-name').value = product_name;
+    document.querySelector('#update-price').value = price;
 }
 
