29ce7e2a0aa957d8778d2de208a36827fa4b8479
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-03-21 14:59:26 +0900

    no message

--- js_sql_work/supabase09-auth/review.js
+++ js_sql_work/supabase09-auth/review.js
@@ -4,7 +4,27 @@ const supabaseAnonkey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYm
 const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonkey);
 
 async function selectReview() {
-    const res = await supabase.from('review').select();
+
+    const params = new URLSearchParams(window.location.search);
+    // console.log("params = "+params);
+
+    const pageNum = params.get('pageNum') || 1;
+    // console.log("pageNum = "+pageNum);
+
+    // const aa = params.get('a');
+    // console.log("aa = "+aa);
+
+    // pageNum 1 0~9
+    // pageNum 2 10~19
+    // pageNum 3 20~29
+
+    const [from, to] = [(pageNum - 1) * 10, (pageNum * 10) - 1];
+    console.log('from = ', from);
+    console.log('to = ', to);
+    const res = await supabase
+                .from('review')
+                .select()
+                .range(from, to);
     console.log(res);
 
     const $reviewList = document.getElementById('review-list');
@@ -13,10 +33,28 @@ async function selectReview() {
 
     res.data.forEach(review => {
         const reviewDiv = document.createElement('div');
-        reviewDiv.innerHTML = `
-            <h3>${review.title}</h3>
-            <img src="${review.file_url}" alt="${review.file_url}" width="100">
-        `
+        reviewDiv.style.display = "flex";
+        reviewDiv.style.border = "1px solid black";
+        reviewDiv.style.borderCollapse = "collapse";
+
+        if (review.file_url === null) {
+            reviewDiv.innerHTML = `
+                <div style='width:150px; padding:1rem;border-right:1px solid black;'>
+                    <h3>${review.title}</h3>
+                </div>
+                <div style='padding:1rem;'>
+                </div>
+            `
+        } else {
+            reviewDiv.innerHTML = `
+                <div style='width:150px; padding:1rem;border-right:1px solid black;'>
+                    <h3>${review.title}</h3>
+                </div>
+                <div style='padding:1rem;'>
+                    <img src="${review.file_url}" alt="${review.file_url}" width="200">
+                </div>
+            `
+        }
         $reviewList.appendChild(reviewDiv);
     })
 
