4f370171130c0aee538318b7fdfdb20060f810e5
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-03-26 16:41:49 +0900

    no message

--- react_work/ex0326/src/App.jsx
+++ react_work/ex0326/src/App.jsx
@@ -11,7 +11,7 @@ const supabase = createClient(url, pwd);
 
 function App() {
   const [count, setCount] = useState(0);
-  const [pages, setPages] = useState([1, 2, 3]);
+  const [pages, setPages] = useState([1, 2, 3, 4, 5]);
 
   const [data, setData] = useState([
     { review_num: 1, user_id: "40dcf945-4749-4891-8be1-cd7a431e39e3", name: "홍길동", title: "제목1", review_txt: "리뷰입니다11." },
@@ -20,25 +20,37 @@ function App() {
   ]);
 
   // 처음에 한번만 콜백함수 호출해라...
-  useEffect(async () => {
-    // 총 페이지 개수 구하기
-    const total = (await supabase.from('review').select()).data.length;
-    let totalPage = Math.ceil(total / 5);// 29/5 = 5.XXX
+  useEffect(() => {
+    const fetchData = async()=>{
+      // 총 페이지 개수 구하기
+      const total = (await supabase.from('review').select()).data.length;
+      let totalPage = Math.ceil(total / 5);// 29/5 = 5.XXX 6 [1,2,3,4,5,6] 7 [1,2,3,4,5,6,7]
 
-    const params = new URLSearchParams(location.search);
-    const pageNum = params.get('pageNum') ?? "1";
+      const temp = [];
+      for (let i = 0; i < totalPage; i++) {
+        temp.push(i + 1);
+      }
+      setPages(temp);
 
-    // 1 -> 0, 4
-    // 2 -> 5, 9
-    // 3 -> 10, 14
-    // 4 -> 15 19
-    // 5 -> 20 24
+      // const temp = Array.from({ length: totalPage }, (_, i) => i + 1);
+      // console.log(`temp = `);
+      // console.log(temp);
 
-    const [from, to] = [(parseInt(pageNum) - 1) * 5, parseInt(pageNum) * 5 - 1]
+      const params = new URLSearchParams(location.search);
+      const pageNum = params.get('pageNum') ?? "1";
 
-    const res = await supabase.from('review').select().range(from, to);
-    setData(res.data);
+      // 1 -> 0, 4
+      // 2 -> 5, 9
+      // 3 -> 10, 14
+      // 4 -> 15 19
+      // 5 -> 20 24
 
+      const [from, to] = [(parseInt(pageNum) - 1) * 5, parseInt(pageNum) * 5 - 1]
+
+      const res = await supabase.from('review').select().range(from, to);
+      setData(res.data);
+    }
+    fetchData();
   }, []);
 
   console.log("호출됨");
@@ -60,7 +72,7 @@ function App() {
       <div style={{ display: 'flex', gap: '1rem' }}>
         {
           pages.map((item) => (
-            <a href={`?pageNum=${item}`}>{item}</a>
+            <a key={item} href={`?pageNum=${item}`}>{item}</a>
           ))
         }
       </div>
