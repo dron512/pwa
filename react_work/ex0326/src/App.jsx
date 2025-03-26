9d0e2e23c199dce6535dc1dd7370fc83ec36811a
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-03-26 16:04:37 +0900

    no message

--- react_work/ex0326/src/App.jsx
+++ react_work/ex0326/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { createClient } from '@supabase/supabase-js';
 
 // useState useEffect useRef useMemo useCallback 훅
 // 값이 변경될시에 화면이 재랜더링
 
+const url = "https://zgrjjnifqoactpuqolao.supabase.co";
+const pwd = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpncmpqbmlmcW9hY3RwdXFvbGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyNDc0NTgsImV4cCI6MjA1NjgyMzQ1OH0._Vl-6CRKdMjeDRyNoxlfect7sgusZ7L0N5OYu0a5hT0"
+const supabase = createClient(url, pwd);
+
 function App() {
   const [count, setCount] = useState(0);
+  const [pages, setPages] = useState([1, 2, 3]);
 
   const [data, setData] = useState([
     { review_num: 1, user_id: "40dcf945-4749-4891-8be1-cd7a431e39e3", name: "홍길동", title: "제목1", review_txt: "리뷰입니다11." },
@@ -13,22 +19,54 @@ function App() {
     { review_num: 3, user_id: "40dcf945-4749-4891-8be1-cd7a431e39e3", name: "이길동", title: "제목3", review_txt: "리뷰입니다33." }
   ]);
 
+  // 처음에 한번만 콜백함수 호출해라...
+  useEffect(async () => {
+    // 총 페이지 개수 구하기
+    const total = (await supabase.from('review').select()).data.length;
+    let totalPage = Math.ceil(total / 5);// 29/5 = 5.XXX
+
+    const params = new URLSearchParams(location.search);
+    const pageNum = params.get('pageNum') ?? "1";
+
+    // 1 -> 0, 4
+    // 2 -> 5, 9
+    // 3 -> 10, 14
+    // 4 -> 15 19
+    // 5 -> 20 24
+
+    const [from, to] = [(parseInt(pageNum) - 1) * 5, parseInt(pageNum) * 5 - 1]
+
+    const res = await supabase.from('review').select().range(from, to);
+    setData(res.data);
+
+  }, []);
+
+  console.log("호출됨");
+
   // const retData = data.map(item => {
   //   // console.log(item);
   //   return `<li>내용</li>`;
   // })
   // console.log(retData);
 
-  console.log("App 랜더링");
-
-  const getReview = ()=>{
-    console.log("test");
+  // useState 를 사용하게 되면 재랜더링이 일어난다.
+  const getReview = async () => {
+    const res = await supabase.from('review').select();
+    setData(res.data);
   }
 
   return (
     <>
+      <div style={{ display: 'flex', gap: '1rem' }}>
+        {
+          pages.map((item) => (
+            <a href={`?pageNum=${item}`}>{item}</a>
+          ))
+        }
+      </div>
       <h1>count = {count}</h1>
       <button onClick={() => { setCount(count + 1) }}>count증가</button>
+      <button onClick={getReview}>review데이터 가져오기</button>
       {
         data.map(item => (
           <div key={item.review_num}>
@@ -38,7 +76,6 @@ function App() {
           </div>
         ))
       }
-      <button onClick={getReview}>review데이터 가져오기</button>
     </>
   )
 }
