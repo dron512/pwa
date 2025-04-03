7afa5e04b22bcd1a209ae9baa627671f27aad196
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-04-03 12:22:09 +0900

    no message

--- javascript_work/0403/ex07.js
+++ javascript_work/0403/ex07.js
@@ -2,6 +2,23 @@ const a = "abc";
 
 console.log(a.repeat(2));
 // 강제 종료 되면 종료 구문이 실행되지 않음
-console.log("abc".repeat(-1));
+// console.log("abc".repeat(-1));
+// console.log("종료되었습니다.");
 
-console.log("종료되었습니다.");
+const str = "Hello Hello";
+           //hello hello
+
+console.log(str.replace(/hello/ig, 'LEE' ));
+
+// RnD 무료
+
+// 텍스트 전처리 -> python 서울대.. 대학원..
+// 안뇽하세요 -> 안녕하세요
+
+// 음성 
+
+// 카멜케이스
+let todoList; // -> todo_list
+
+// 스네이크케이스
+let todo_list; // -> todoList
