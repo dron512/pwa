92364b012674162313329f668e4a51c7dc49fda1
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-04-09 09:36:38 +0900

    250409

--- react_work/ex0407/vite.config.js
+++ react_work/ex0407/vite.config.js
@@ -1,21 +1,47 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.dhlottery.co.kr',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/cloud': {
-        target: 'https://port-0-pwa-m913h1zh8f5530cc.sel4.cloudtype.app/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cloud/, '')
-      }
+    plugins: [react(), VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'robots.txt'],
+        manifest: {
+            name: 'My PWA App',
+            short_name: 'PWAApp',
+            description: 'My Vite + React PWA!',
+            theme_color: '#ffffff',
+            background_color: '#ffffff',
+            display: 'standalone',
+            start_url: '/',
+            icons: [
+                {
+                    src: '/icon-192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: '/icon-512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+            ],
+        },
+    }),
+    ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://www.dhlottery.co.kr',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            },
+            '/cloud': {
+                target: 'https://port-0-pwa-m913h1zh8f5530cc.sel4.cloudtype.app/',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/cloud/, '')
+            }
+        }
     }
-  }
 })
