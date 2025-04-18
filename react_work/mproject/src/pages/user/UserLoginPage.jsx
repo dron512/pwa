d6b55a06edbeebbc07e64d78d72dbf2e951aee8a
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-04-18 15:48:40 +0900

    no message

--- react_work/mproject/src/pages/user/UserLoginPage.jsx
+++ react_work/mproject/src/pages/user/UserLoginPage.jsx
@@ -1,16 +1,31 @@
 import React, {useState} from 'react';
-import {Button, Flex, Form, Input, Layout} from "antd";
+import {Button, Flex, Form, Input, Layout, message} from "antd";
 import {loginUser} from "../../database/userManager.js";
+import bcrypt from "bcryptjs";
+import {useNavigate} from "react-router-dom";
 
 const {Content} = Layout;
 
 function UserLoginPage(props) {
+    const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
-    const finish = (values) => {
+    const finish = async (values) => {
         const {email, password} = values;
         setLoading(true);// 로그인 못하게 막음
         // userManager안에 loginUser 함수 호출
-        loginUser(email, password);
+        const ret = await loginUser(email, password);
+        if(ret.message === 'email'){
+            message.error('이메일을 확인하세요');
+        }else{
+            const isMatch = await bcrypt.compare(password, ret.data.password);
+            if(isMatch){
+                message.success('로그인성공하였습니다.');
+                navigate('/');
+            }
+            else{
+                message.error('패스워드를 확인하세요');
+            }
+        }
         setLoading(false);// 로그인 할 수 있게 풀음
     }
 
@@ -18,7 +33,7 @@ function UserLoginPage(props) {
         <Content>
             <Flex style={{justifyContent: 'center', alignItems: 'center', height: '100%'}} vertical>
                 <h1 style={{fontSize: '2rem'}}>로그인</h1>
-                <Form onFinish={finish} initialValues={{email: "aaa@naver.com", password: "12345"}}>
+                <Form onFinish={finish} initialValues={{email: "test1@example.com", password: "12345"}}>
                     <Form.Item
                         label="이메일"
                         name="email"
