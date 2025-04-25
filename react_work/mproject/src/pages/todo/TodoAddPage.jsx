0d4183301848d8e53f2fe05b2205640fa21c07d4
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-04-25 15:58:40 +0900

    no message

--- react_work/mproject/src/pages/todo/TodoAddPage.jsx
+++ react_work/mproject/src/pages/todo/TodoAddPage.jsx
@@ -1,11 +1,23 @@
 import React, {useState} from 'react';
-import {Button, Card, Form, Input, Layout} from "antd";
+import {Button, Card, Form, Input, Layout, notification, Select} from "antd";
 
 function TodoAddPage(props) {
     const [loading, setLoading] = useState(false);
-    const onFinish = () => {
+    const onFinish =  (values) => {
         setLoading(true);
-        console.log("저장할꺼임");
+        fetch( 'https://6809e0571f1a52874cde2b14.mockapi.io/todos',{
+            method: 'POST',
+            body: JSON.stringify(values),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .then(data => {
+            notification.success({
+                message: "성공적으로 저장하였습니다."
+            })
+        })
+
         setLoading(false);
     }
     return (
@@ -27,7 +39,15 @@ function TodoAddPage(props) {
                     <Form.Item label="Todo" name="todo" rules={[{required: true, message: "할일을 입력하세요"}]}>
                         <Input/>
                     </Form.Item>
-
+                    <Form.Item label="completed" name="completed" rules={[{required: true, message: "할일 완료여부 선택하세요"}]}>
+                        <Select>
+                            <Select.Option value="false">미완료</Select.Option>
+                            <Select.Option value="true">완료</Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="userID" name="userId" rules={[{required: true, message: "사용자ID를 입력하세요"}]}>
+                        <Input/>
+                    </Form.Item>
                     <Form.Item>
                         <Button type="primary" htmlType="submit" loading={loading} block>저장</Button>
                     </Form.Item>
