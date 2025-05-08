4d1904dc79c7459b98aaf0ca50ae8181ab01cfb8
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-08 16:49:26 +0900

    no message

--- project_work/mapproject/src/App.jsx
+++ project_work/mapproject/src/App.jsx
@@ -1,28 +1,38 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 import {Map, MapMarker, useKakaoLoader} from "react-kakao-maps-sdk";
-import axios from "axios";
+import {fetchCities} from "../api/supadb.js";
+import {fetchAqi} from "../api/airapi.js";
+import {Card, Space} from "antd";
+import AirTable from "./components/AirTable.jsx";
 
 function App() {
     const [count, setCount] = useState(0);
     // 마커 설정할 위도 경도 나중에 supabase 에서 데이터 가져와서 setCities 할 계획
-    const [cities, setCities] = useState([
-        {id: 1, name: "달서구", lat: 35.8296, lng: 128.5328}, // 달서구
-        {id: 2, name: "중구", lat: 35.8693, lng: 128.6062}, // 중구
-        {id: 3, name: "남구", lat: 35.8467, lng: 128.5971}, // 남구
-        {id: 4, name: "동구", lat: 35.8867, lng: 128.6350}, // 동구
-        {id: 5, name: "수성구", lat: 35.8588, lng: 128.6305}, // 수성구
+    const [cities, setCities] = useState([]);
+    const [aqiInfo,setAqiInfo] = useState({});
 
-      // {id: 6, name: "내마음데로", lat: 35.8588, lng: 128.8888} // 수성구
+    const [loading, error] = useKakaoLoader({
+        appkey: import.meta.env.VITE_KAKAO_MAP_KEY,
+        libraries: ["clusterer", "services", "drawing"]
+    });
 
-    ]);
-    // 카카오 api key 설정
-    useKakaoLoader({
-        appkey: 'a38e46e8d7d8c504f896a7933c9e4494',
-    })
+    // supabase 에서 cities 데이터 가져오기
+    useEffect(() => {
+        fetchCities()
+            .then(data => {
+                setCities(data);
+            });
+    }, []);
+
     // 좌표 클릭시 해당 미세먼지 초미세먼지 데이터 가져와서 뿌리기
-    const getAqi = () => {
+    const clickAqi = (city) => {
+        fetchAqi(city.latitude, city.longitude)
+            .then(data => {
+                setAqiInfo(data);
+            });
     }
+
     return (
         <>
             <h1>Hello</h1>
@@ -31,12 +41,15 @@ function App() {
                  style={{width: '100%', height: '80vh'}}>
                 {cities.map((city) => (
                     <MapMarker key={city.id}
-                               position={{lat: city.lat, lng: city.lng}}
-                               onClick={getAqi}
+                               position={{lat: city.latitude, lng: city.longitude}}
+                               onClick={() => {
+                                   clickAqi(city)
+                               }}
                     >
                     </MapMarker>
                 ))}
             </Map>
+            <AirTable {...aqiInfo}></AirTable>
         </>
     )
 }
