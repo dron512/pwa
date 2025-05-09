55f89e726cfce71f4aafefb849bcd779588175d3
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-09 15:54:50 +0900

    no message

--- project_work/mapproject/src/components/AirTable.jsx
+++ project_work/mapproject/src/components/AirTable.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Card, Space, Table } from "antd";
+import { Line } from "@ant-design/plots";
 
 function AirTable(props) {
+  console.log("AirTable props", props);
   const [data, setData] = useState(props);
 
   // porps 값이 바뀌면.. useEffect 가 실행된다.
@@ -31,32 +33,61 @@ function AirTable(props) {
   const dataSource = [
     {
       key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
+      aqi: "0~50",
+      grade: "좋음",
+      mean: "공기가 깨끗",
+      remark: "누구나 활동 OK",
     },
     {
       key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
+      aqi: "51~100	",
+      grade: "보통",
+      mean: "민감군은 주의",
+      remark: "민감군은 실외 활동 조절",
+    },
+    {
+      key: "3",
+      aqi: "101~150",
+      grade: "나쁨",
+      mean: "민감군은 피로감·호흡 불편 가능",
+      remark: "실외 활동 자제 권장",
+    },
+    {
+      key: "4",
+      aqi: "151~200",
+      grade: "매우 나쁨",
+      mean: "누구나 영향 가능",
+      remark: "노약자 실외 활동 제한",
+    },
+    {
+      key: "5",
+      aqi: "201 이상",
+      grade: "위험",
+      mean: "건강에 매우 해로움",
+      remark: "외출 자제, 실내 대기 권장",
     },
   ];
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: "AQI",
+      dataIndex: "aqi",
+      key: "aqi",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "등급",
+      dataIndex: "grade",
+      key: "grade",
     },
     {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
+      title: "의미",
+      dataIndex: "mean",
+      key: "mean",
+    },
+    ,
+    {
+      title: "비고",
+      dataIndex: "remark",
+      key: "remark",
     },
   ];
 
@@ -64,17 +95,54 @@ function AirTable(props) {
 
   return (
     <Card hoverable style={{ margin: "2rem" }}>
-      <h1>대기질 정보</h1>
-      <p>
-        {/* <h2>일산화탄소 : {JSON.stringify(data)} </h2> */}
-        <h2>일산화탄소 : {data?.iaqi?.co?.v} </h2>
-        <h2>이산화질소 : {data?.iaqi?.no2?.v} </h2>
-        <h2>오존 : {data?.iaqi?.o3?.v} </h2>
-        <h2>미세먼지 : {data?.iaqi?.pm10?.v} </h2>
-        <h2>초미세먼지 : {data?.iaqi?.pm25?.v} </h2>
-        <h2>아황산가스 : {data?.iaqi?.so2?.v} </h2>
-      </p>
+      <h1>대기질 정보 {data.aqi}</h1>
       <Table dataSource={dataSource} columns={columns} pagination={false} />
+      오염물질 단위 좋음 기준 (AQI 0~50 해당)
+      <br />
+      일산화탄소 (CO) ppm 0~4.4 ppm
+      <br />
+      이산화질소 (NO₂) ppb 0~53 ppb
+      <br />
+      오존 (O₃) ppb (8시간 평균) 0~54 ppb
+      <br />
+      미세먼지 (PM10) μg/m³ 0~54 μg/m³
+      <br />
+      초미세먼지 (PM2.5) μg/m³ 0~12 μg/m³
+      <br />
+      아황산가스 (SO₂) ppb 0~35 ppb
+      <br />
+      <div>
+        {/* <h2>일산화탄소 : {JSON.stringify(data)} </h2> */}
+        <h2>일산화탄소 : {data?.iaqi?.co?.v} μg/m³</h2>
+        <h2>이산화질소 : {data?.iaqi?.no2?.v} μg/m³</h2>
+        <h2>오존 : {data?.iaqi?.o3?.v} μg/m³</h2>
+        <h2>미세먼지 : {data?.iaqi?.pm10?.v} μg/m³</h2>
+        <h2>초미세먼지 : {data?.iaqi?.pm25?.v} μg/m³</h2>
+        <h2>아황산가스 : {data?.iaqi?.so2?.v} μg/m³</h2>
+      </div>
+      <Line
+        data={
+            data.forecast?.daily?.pm10
+            ?.map((item, index) => ({
+              date: item.day,
+              value: item.avg,
+              type: "PM10",
+            }))
+            .concat(
+                data.forecast?.daily?.pm25?.map((item) => ({
+                date: item.day,
+                value: item.avg,
+                type: "PM2.5",
+              }))
+            ) || []
+        }
+        xField="date"
+        yField="value"
+        colorField="type"
+        point={{
+          shape: "diamond",
+        }}
+      />
     </Card>
   );
 }
