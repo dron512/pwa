6df249080e968f9d6e38fbf3eb93508eda688eb2
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-09 16:18:55 +0900

    no message

--- project_work/mapproject/src/App.jsx
+++ project_work/mapproject/src/App.jsx
@@ -6,12 +6,90 @@ import AirTable from "./components/AirTable.jsx";
 import { Bar, Line, Pie, Column } from "@ant-design/plots";
 
 const DemoDefaultTooltip = () => {
+    const data = [
+        {
+            "name": "London",
+            "月份": "Jan.",
+            "月均降雨量": 18.9
+        },
+        {
+            "name": "London",
+            "月份": "Feb.",
+            "月均降雨量": 28.8
+        },
+        {
+            "name": "London",
+            "月份": "Mar.",
+            "月均降雨量": 39.3
+        },
+        {
+            "name": "London",
+            "月份": "Apr.",
+            "月均降雨量": 81.4
+        },
+        {
+            "name": "London",
+            "月份": "May",
+            "月均降雨量": 47
+        },
+        {
+            "name": "London",
+            "月份": "Jun.",
+            "月均降雨量": 20.3
+        },
+        {
+            "name": "London",
+            "月份": "Jul.",
+            "月均降雨量": 24
+        },
+        {
+            "name": "London",
+            "月份": "Aug.",
+            "月均降雨量": 35.6
+        },
+        {
+            "name": "Berlin",
+            "月份": "Jan.",
+            "月均降雨量": 12.4
+        },
+        {
+            "name": "Berlin",
+            "月份": "Feb.",
+            "月均降雨量": 23.2
+        },
+        {
+            "name": "Berlin",
+            "月份": "Mar.",
+            "月均降雨量": 34.5
+        },
+        {
+            "name": "Berlin",
+            "月份": "Apr.",
+            "月均降雨量": 99.7
+        },
+        {
+            "name": "Berlin",
+            "月份": "May",
+            "月均降雨量": 52.6
+        },
+        {
+            "name": "Berlin",
+            "月份": "Jun.",
+            "月均降雨量": 35.5
+        },
+        {
+            "name": "Berlin",
+            "月份": "Jul.",
+            "月均降雨量": 37.4
+        },
+        {
+            "name": "Berlin",
+            "月份": "Aug.",
+            "月均降雨量": 42.4
+        }
+    ];
   const config = {
-    data: {
-      type: "fetch",
-      value:
-        "https://gw.alipayobjects.com/os/antfincdn/iPY8JFnxdb/dodge-padding.json",
-    },
+    data,
     xField: "月份",
     yField: "月均降雨量",
     colorField: "name",
@@ -95,7 +173,7 @@ function App() {
       {/* <Line {...config} /> */}
       <Line data={config.data} xField={config.xField} yField={config.yField} />
       <Bar data={config.data} xField={config.xField} yField={config.yField} />
-      <Pie data={config.data} xField={config.xField} yField={config.yField} />
+      <Pie data={config.data} xField={config.xField} yField={config.yField} colorField={config.yField} angleField={config.yField}/>
       <DemoDefaultTooltip />
     </>
   );
