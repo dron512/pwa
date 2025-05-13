76ede39ddcc7674b83c8c79c55bcf466803bdba3
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-13 16:01:37 +0900

    no message

--- project_work/mapproject/src/components/Reviews.jsx
+++ project_work/mapproject/src/components/Reviews.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { fetchReviews } from "../../api/supadb";
-import { UserOutlined, EnvironmentOutlined } from '@ant-design/icons';
+import { UserOutlined, EnvironmentOutlined } from "@ant-design/icons";
+import { Card, Typography, Form, Input, Button, message, Rate } from "antd";
+// import { useForm } from "antd/es/form/Form";
+
+const { Title, Text } = Typography;
+const { TextArea } = Input;
 
 const Reviews = ({ city }) => {
   if (!city) {
@@ -14,37 +19,82 @@ const Reviews = ({ city }) => {
   // useEffect(() => {
   //   setCity(propCity);
   // }, [propCity]);
-  const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = useState(null);
 
   useEffect(() => {
     fetchReviews(city.id)
       .then((data) => {
         setReviews(data); // 화면 재랜더링
       })
-      .catch(e=>{
+      .catch((e) => {
         console.log(e);
       });
   }, [city]);
+
+  // custom 훅은 맨 마지막에 호출되야 한다고 경고 메시지 떠서 옮겼습니다.
+  const [form] = Form.useForm();
+
+  const handleSubmit = (values) => {
+    console.log("submit");
+    console.log(values);
+  };
 
   return (
     <div>
       <h1>Reviews {city.name}</h1>
       <h2>미세먼지 {city.aqi}</h2>
-      { reviews.map((review) => {
-        console.log(review);
-        return (
-            <div key={review.id}>
-              <p>{review.comment}</p>
-              <p> 
-                <UserOutlined />
-                 작성자: {review.user_name}</p>
-              <p>작성일: {new Date(review.created_at).toLocaleDateString()}</p>
-            </div>
-        );
-      }
-    )}
+      {reviews &&
+        reviews.map((review) => (
+          <div key={review.id}>
+            <p>{review.comment}</p>
+            <p>
+              <UserOutlined />
+              작성자: {review.user_name}
+            </p>
+            <p>작성일: {new Date(review.created_at).toLocaleDateString()}</p>
+          </div>
+        ))}
+      <Card>
+        <Title level={3}>
+          <EnvironmentOutlined />
+          리뷰작성
+        </Title>
+        {
+          <Form form={form} onFinish={handleSubmit} layout="vertical">
+            <Form.Item
+              name="userName"
+              label="이름"
+              rules={[{ required: true, message: "이름을 입력하세요" }]}
+            >
+              <Input
+                prefix={<UserOutlined />}
+                placeholder="이름을 입력하세요"
+              />
+            </Form.Item>
+            <Form.Item
+              name="rating"
+              label="평점"
+              rules={[{ required: true, message: "평점을 선택해주세요" }]}
+            >
+              <Rate></Rate>
+            </Form.Item>
+            <Form.Item
+              name="comment"
+              label="리뷰 내용"
+              rules={[{ required: true, message: "리뷰 내용을 입력해주세요" }]}
+            >
+              <TextArea rows={4} placeholder="리뷰 내용을 입력해주세요" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>리뷰 작성</Button>
+            </Form.Item>
+          </Form>
+        }
+      </Card>
     </div>
   );
 };
+
+Reviews.__isStatic = true;
 
 export default Reviews;
