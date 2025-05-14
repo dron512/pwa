47c2ed11b14fdd9b7482de37676bac828ef7cd82
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-14 16:01:23 +0900

    no message

--- project_work/mapproject/src/App.jsx
+++ project_work/mapproject/src/App.jsx
@@ -1,75 +1,12 @@
-import { useEffect, useState } from "react";
-import { fetchCities } from "../api/supadb.js";
-import { fetchAqi } from "../api/airapi.js";
-import { Button, Drawer } from "antd";
-
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import RootPage from "./pages/RootPage.jsx";
 import UserPage from "./pages/UserPage.jsx";
+import Menu from "./components/Menu.jsx";
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const [placement, setPlacement] = useState("left");
-  const navigate = useNavigate();
-
-  const showDrawer = () => {
-    setOpen(true);
-  };
-  const onClose = () => {
-    setOpen(false);
-  };
-  const onChange = (e) => {
-    setPlacement(e.target.value);
-  };
-
   return (
     <>
-      <div
-        style={{ position: "absolute", top: "1rem", left: "1rem", zIndex: 100 }}
-      >
-        <Button type="primary" onClick={showDrawer}>
-          Open
-        </Button>
-      </div>
-      <Drawer
-        title="메뉴입니다."
-        placement={placement}
-        closable={false}
-        onClose={onClose}
-        open={open}
-        key={placement}
-      >
-        <div>
-          <ul>
-            <li>
-              <Button
-                onClick={() => {
-                  navigate("/");
-                  setOpen(false);
-                }}
-              >
-                root
-              </Button>
-            </li>
-            <li>
-              <Button
-                onClick={() => {
-                  navigate("/user");
-                  setOpen(false);
-                }}
-              >
-                user
-              </Button>
-            </li>
-            {/* <li>
-              <Link to="/user">user</Link>
-            </li> */}
-          </ul>
-        </div>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Drawer>
-
+      <Menu />
       <Routes>
         <Route path="/" element={<RootPage />}></Route>
         <Route path="/user" element={<UserPage />}></Route>
