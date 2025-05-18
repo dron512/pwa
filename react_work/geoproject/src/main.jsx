58e4b05af66ac1fe7eb9dd0e3855cf40e881d10f
dron512 <parkmyounghoi@gmail.com>
2025-05-18 18:27:56 +0900

    no message

--- react_work/geoproject/src/main.jsx
+++ react_work/geoproject/src/main.jsx
@@ -1,7 +1,20 @@
+import '@ant-design/v5-patch-for-react-19';
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import { unstableSetRender } from 'antd';
+import { createRoot } from 'react-dom/client';
+
+unstableSetRender((node, container) => {
+  container._reactRoot ||= createRoot(container);
+  const root = container._reactRoot;
+  root.render(node);
+  return async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    root.unmount();
+  };
+});
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
