3cd7e5f79ebf4db2eeb7034666be7e2d84135071
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-21 17:59:37 +0900

    no message

--- nodejs-book-master/ch9/9.5/nodebird/controllers/post.js
+++ nodejs-book-master/ch9/9.5/nodebird/controllers/post.js
@@ -1,4 +1,4 @@
-const { Post, Hashtag } = require('../models');
+const { Post, Hashtag } = require("../models");
 
 exports.afterUploadImage = (req, res) => {
   console.log(req.file);
@@ -15,15 +15,22 @@ exports.uploadPost = async (req, res, next) => {
     const hashtags = req.body.content.match(/#[^\s#]*/g);
     if (hashtags) {
       const result = await Promise.all(
-        hashtags.map(tag => {
+        hashtags.map((tag) => {
           return Hashtag.findOrCreate({
             where: { title: tag.slice(1).toLowerCase() },
-          })
-        }),
+          });
+        })
       );
-      await post.addHashtags(result.map(r => r[0]));
+      console.log("헐 저건 뭐지");
+      console.log(result);
+      result.map((row)=>{
+        console.log(row);
+        console.log('row[0]');
+        console.log(row[0]);
+      })
+      await post.addHashtags(result.map((r) => r[0]));
     }
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
     console.error(error);
     next(error);
