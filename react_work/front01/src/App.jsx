371e4ff01c8b4c58f68565e9c02a44cea71e2c45
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-22 12:19:21 +0900

    no message

--- react_work/front01/src/App.jsx
+++ react_work/front01/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [users, setUsers] = useState(dbusers);
 
   const getUsers = async (event) => {
-    const result = await axios.get('http://localhost:8080/users')
+    const result = await axios.get('https://port-0-pwa-m913h1zh8f5530cc.sel4.cloudtype.app')
     const {data, status} = result;
 
     setUsers(data);
