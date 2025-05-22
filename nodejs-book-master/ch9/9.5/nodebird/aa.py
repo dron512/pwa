6508a3e5f1cf626dcfb3a33e58511a60b7007044
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-22 17:43:09 +0900

    no message

---
+++ nodejs-book-master/ch9/9.5/nodebird/aa.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+pyautogui.moveTo(500, 500)
+pyautogui.write("Hello, world!")
+
--- nodejs-book-master/ch9/9.5/nodebird/controllers/user.js
+++ nodejs-book-master/ch9/9.5/nodebird/controllers/user.js
@@ -1,16 +1,45 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.follow = async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { id: req.user.id } });
-    if (user) { // req.user.id가 followerId, req.params.id가 followingId
+    if (user) {
+      // req.user.id가 followerId, req.params.id가 followingId
       await user.addFollowing(parseInt(req.params.id, 10));
-      res.send('success');
+      res.send("success");
     } else {
-      res.status(404).send('no user');
+      res.status(404).send("no user");
     }
   } catch (error) {
     console.error(error);
     next(error);
   }
+};
+
+exports.followingDisconnect = async (req, res, next) => {
+  console.log("req");
+  console.log(req.user);
+  const user = await User.findOne({
+    where: { id: req.user.id },
+    include: [
+      {
+        model: User,
+        attributes: ["id", "nick"],
+        as: "Followers",
+      },
+      {
+        model: User,
+        attributes: ["id", "nick"],
+        as: "Followings",
+      },
+    ],
+  });
+  if (user) {
+    console.log("user.Follwings")
+    console.log(user.Followings);
+
+    user.removeFollowing(req.params.id);
+    
+  }
+  res.send("no user" + req.params.id);
 };
