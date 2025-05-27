0ccb39c0ce2544fcd1c70a58d5316c1e72d6c3df
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-05-27 12:44:52 +0900

    no message

--- react_work/front02/src/App.jsx
+++ react_work/front02/src/App.jsx
@@ -6,34 +6,15 @@ const API_URL = import.meta.env.VITE_API_NODE_URL;
 
 function App() {
 
-
-
   const [test,setTest] = useState("안녕 test");
   const getRoot = async () => {
-    // console.log("get root");
-    // const result = await axios.get(API_URL);
-    // console.log(result.data);
-
     const {data, error} = await axios.get(`${API_URL}`);
     console.log(data);
     console.log(error);
-
     setTest(data);
-
-    // 화면 랜더링 아님 그냥 메모리에 test 변수에다가 data로 값 바꾸는 거...
-    // 화면 랜더링 할려면 useState사용해야함..
-    // test = data;
-    // document.getElementById('aa').innerHTML = data;
   }
 
-  // useEffect(() => {
-  //   document.getElementById('aa').innerHTML = test;
-  // }, []);
-
-  useEffect(() => {
-
-    // 푸시 알람 받을 준비 됐어 할꺼야
-
+  const daeguSub = ()=>{
     if ("serviceWorker" in navigator && "PushManager" in window) {
       console.log("service worker");
       navigator.serviceWorker.ready.then((registration) => {
@@ -45,6 +26,8 @@ function App() {
             applicationServerKey: "BMCne_9uf301kxUf_wqVTiARfQ0t9tnbTzw7WDLu-eeJ0QGxM_AxkXbEAWxaqSLkNa7ty-XZKjYX5EHLw63N7Y4",
           })
           .then((subscription) => {
+            subscription.city = '대구';
+
             return fetch(`${API_URL}/subscribe`, {
               method: "POST",
               body: JSON.stringify(subscription),
@@ -57,14 +40,14 @@ function App() {
             console.error("푸시 구독 실패:", error);
           });
       });
-
     }
-  }, []);
+  }
 
   return (
     <>
       <h1>Hello React</h1>
       <p id="aa">Hello React + {test}</p>
+      <button onClick={daeguSub}>대구구독</button>
       {/*<button onClick={getRoot}>백엔드요청</button>*/}
       <button onClick={
         () => {
