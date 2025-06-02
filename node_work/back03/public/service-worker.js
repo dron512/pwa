4cad1353164444911bc8e2def30c177d85163987
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-06-02 16:13:28 +0900

    no message

---
+++ node_work/back03/public/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+];
+
+// 설치 단계: 캐시 저장
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 요청 가로채기
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
