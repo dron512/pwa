03512460cc12777829393d11c134c9930fe8dbda
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-06-11 13:00:52 +0900

    no message

--- node_work/back05/routes/GisaRouter.js
+++ node_work/back05/routes/GisaRouter.js
@@ -3,11 +3,16 @@ const supabase = require("../config/supa.js");
 
 router = express.Router();
 
-router.get("", async (req, res, next) => {
+router.route("")
+.get(async (req, res, next) => {
   const { data, error } = await supabase.from("ice_res").select("*");
 
   // gisa.html 로 가면서 select 한 데이터도 가지고 가라.
   return res.render("gisa", { data });
+})
+.post(async(req,res,next)=>{
+    console.log(req.body);
+    return res.send('저장됨');
 });
 
 module.exports = router;
