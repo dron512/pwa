05d4e2abd75f03bccb8590acec50cab30ce86d45
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-06-25 13:08:44 +0900

    no message

---
+++ cs_work/chap03/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = 10;
+            MessageBox.Show($"number = {number}");
+            MessageBox.Show($"number = {number++}");
+            MessageBox.Show($"number = {number--}");
+            MessageBox.Show($"number = {number}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string input = Console.ReadLine();
+            MessageBox.Show($"input = {input}");
+
+            // MessageBox.Show("Hello, World!");
+            // 함수는 매개변수로 스트링을 받기 때문에
+            // string 타입으로 변환해야 합니다.
+
+            // typeof() number() string() 빌튼인 함수
+            // messageBox.Show("Hello, World!".ToString());
+            MessageBox.Show((!true).ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            doA();
+            doA(100);//ㅋㅋㅋㅋㅋㅋ
+            doA(100, 200);
+            doA("Hello");
+            //doA(true);
+        }
+
+        // 함수명이 동일할때는 다르게 만들려고 하면
+        // 매개변수의 개수나 타입이 달라야 합니다.
+        // 함수 오버로딩(Overloading) 이라고 합니다
+        public void doA()
+        {
+            MessageBox.Show("doA() 함수");
+        }
+        public void doA(int aa)
+        {
+            MessageBox.Show($"doA() 함수 aa= {aa}");
+        }
+        public void doA(int aa,int bb)
+        {
+            MessageBox.Show($"doA() 함수 aa= {aa} bb={bb}");
+        }
+        public void doA(string cc)
+        {
+            MessageBox.Show($"doA() 함수 cc= {cc}");
+        }
+    }
+}
--- cs_work/chap03/conapp01/Program.cs
+++ cs_work/chap03/conapp01/Program.cs
@@ -4,12 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// cs-> 0,1
+// 컴파일러
+// 컴파일은 ->  컴퓨터가 이해할 수 있는 언어로 변환하는 것
+// 컴퓨터는 0,1만 압니다.
+
+// 0,1-> 사람이 알수 있는 언어로 cs 변경하는
+// 디컴파일러
+// 디컴파일은 -> 컴퓨터가 이해할 수 있는 언어로 변환된 것을
+// 사람이 이해할 수 있는 언어로 변환하는 것
+
 namespace conapp01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // 32bit 에서는 큰 숫자를 표현할 수 없다.
+            // int는 6,553,535까지 표현 가능
+
+            // long은 9,223,372,036,854,775,807까지 표현 가능
+            // 프로그래밍 언어 언어..
+
+            // L 는 long 타입을 의미한다.
+
+            long b = 12812837912837L;
+            int a = (int)b;
+
+            // int 32bit -> 4byte
+            // long 64bit
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            // 암묵적 형변환(Implicit Casting)
+            int c = 12127389;
+            long d = c;
+
+            // 명시적 형변환(Explicit Casting)
+            long e = 121273833339L;
+            int f = (int)e;
+
+            AA aa = new AA();
+            BB bb = new BB();
+
+            aa.Test();
+            bb.Test();
+
+            AA ab = new BB(); // 업캐스팅
+            ab.Test();
+        }
+    }
+
+    class AA
+    {
+        public virtual void Test()
+        {
+            Console.WriteLine("AA 클래스의 Test 메서드");
+        }
+    }
+    class BB : AA
+    {
+        public override void Test()
+        {
+            Console.WriteLine("BB 클래스의 Test 메서드");
         }
     }
 }
