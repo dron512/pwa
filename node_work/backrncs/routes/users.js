81a63250b9a7713f7466147d99d2568caed6d65c
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-07-10 14:53:45 +0900

    no message

--- node_work/backrncs/routes/users.js
+++ node_work/backrncs/routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../schemas/users');
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res, next) => {
   try {
     const {nickname, email, password} = req.body;
-    const user = new User({email, password, nickname});
+    const hashed = bcrypt.hash(password, 12);
+    const user = new User({email, password: hashed, nickname});
     // insert...
     const result = await user.save();
     return res.json(result);
@@ -28,14 +30,14 @@ router.put('/:id', async (req, res, next) => {
   try {
     const {id} = req.params;
     const {nickname, email, password} = req.body;
-    console.log(nickname, email, password);
+    const hashed = bcrypt.hash(password, 12);
     const updated = await User.findByIdAndUpdate(
       id,
-      {nickname, email, password},
+      {nickname, email, password: hashed},
       {new: true, runValidators: true}
     )
     if (!updated) {
-      return res.status(404).json({message:'사용자를 찾을수 없습니다.'})
+      return res.status(404).json({message: '사용자를 찾을수 없습니다.'})
     }
     return res.json(updated);
   } catch (err) {
