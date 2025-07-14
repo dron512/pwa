ec50b0bc641832e1845d11539caa17f38341da81
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-07-14 11:55:50 +0900

    no message

--- node_work/backrncs/app.js
+++ node_work/backrncs/app.js
@@ -13,13 +13,10 @@ const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const app = express();
 app.use(cors(
-  // {
-  //   origin: 'http://localhost:5173',
-  //   credentials: true, // 쿠키값 허용 하겠다
-  // }
 ));
 app.use(logger('dev'));
 app.use(express.json({limit: '50mb'}));
@@ -40,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 app.listen(3030, () => {
   console.log("Server is running on port 3030");
--- node_work/backrncs/routes/auth.js
+++ node_work/backrncs/routes/auth.js
@@ -29,11 +29,12 @@ router.post('/login', async (req, res) => {
     if (!isMatch) {
       return res.status(401).json({error: 'password does not match'});
     }
-
+  // payload 는 데이터 보이는 거
     const token = jwt.sign({
         userId: user._id,
         nickName: user.nickname,
       },
+      // 비밀번호 유효한지 검사하기 위해
       "secret"
       , {expiresIn: "1h"}
     )
